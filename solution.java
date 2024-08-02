f2f8b4cac9d98fbbdaa5799bf032e17220191546
Kooreis <kooreis.official@gmail.com>
2024-08-02 05:54:41 -0500

    Added the 'generateSubsets' method which will be used to generate all subsets of the list. Currently, it only initializes an empty list of subsets.

--- solution.java
+++ solution.java
@@ -1,9 +1,11 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) {
         List<Integer> list = Arrays.asList(1, 2, 3);
+        List<List<Integer>> subsets = generateSubsets(list);
+    }
+
+    public static List<List<Integer>> generateSubsets(List<Integer> list) {
+        List<List<Integer>> subsets = new ArrayList<>();
+        return subsets;
     }
 }
