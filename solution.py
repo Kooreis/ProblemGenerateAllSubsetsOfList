3d33055a89b86b22cd0d0bee2a7a1d582d84d372
Kooreis <kooreis.official@gmail.com>
2024-08-02 05:54:41 -0500

    In the 'power_set' function, added an else block that initializes an empty list 'main_subset'. It then iterates over the power set of the input list excluding the first element. For each small subset in this power set, it adds the small subset and the first element of the input list combined with the small subset to 'main_subset'. Finally, it returns 'main_subset'.

--- solution.py
+++ solution.py
@@ -1,4 +1,6 @@
-def power_set(input_list):
-    # An empty list is the base case for recursion
-    if len(input_list) == 0:
-        return [[]]
+else:
+        main_subset = []
+        for small_subset in power_set(input_list[1:]):
+            main_subset += [small_subset]
+            main_subset += [[input_list[0]] + small_subset]
+        return main_subset
